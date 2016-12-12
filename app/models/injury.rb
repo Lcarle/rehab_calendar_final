0fa40a741d4669e2024b66eb7a403279c3332724
Lcarle <lcarle@users.noreply.github.com>
2016-12-12 01:19:41 -0600

    v4.3
    v4.3

--- app/models/injury.rb
+++ app/models/injury.rb
@@ -2,6 +2,7 @@ class Injury < ActiveRecord::Base
 
   validates :user_id, :presence => true
   validates :injury_date, :presence => true
+  validates :begin_rehab_on, :presence => true
 
 
   belongs_to :user
